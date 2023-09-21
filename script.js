65d459eee8eefb5d063ea3c31460825652c10129
Yurii Pronin <ProninYurij@i.ua>
2023-09-21 13:38:29 +0300

    work with deploy trouble

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 if (!window.location.search.includes('a=') && !window.location.search.includes('b=')) {
-  window.location = '/index?a=1&b=2';
+  window.location = '/index.html';
 }
 
 function handleSubmit(event) {
